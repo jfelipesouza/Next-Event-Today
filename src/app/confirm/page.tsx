8ba6c910cc0a13d453a3accbb98f7cc9c5042068
Felipe Souza <j.felipe.dev.18v@gmail.com>
2023-11-20 20:32:20 -0300

    update

--- src/app/confirm/page.tsx
+++ src/app/confirm/page.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import Image from 'next/image'
+import Link from 'next/link'
 import { jwtDecode } from 'jwt-decode'
 
 import Header from '@/components/header'
@@ -55,11 +56,29 @@ const Confirm: React.FC = () => {
   return (
     <>
       <Header />
-      <main className="w-screen h-screen flex flex-col items-center bg gap-9 ">
+      <main className="w-screen h-screen ">
         {loading ? (
-          <></>
+          <div className="flex w-full items-center justify-center h-screen bg-slate-200 text-2xl gap-4 text-secundary-500 ">
+            <p>LOADING...</p>
+            <div className="animate-bounce mb-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="h-12 w-12 text-secundary-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 14v2a3 3 0 01-3 3H8a3 3 0 01-3-3v-2m0 0a8 8 0 018-8h0a8 8 0 018 8z"
+                />
+              </svg>
+            </div>
+          </div>
         ) : (
-          <>
+          <div className="w-full h-full gap-10 flex flex-col items-center mt-8">
             <div className="min-w-[300px] min-h-[300px] w-[50vw] h-[25vw] relative my-4">
               {status ? (
                 <Image alt="sad face" src={'/happy.svg'} fill />
@@ -67,14 +86,28 @@ const Confirm: React.FC = () => {
                 <Image alt="sad face" src={'/sad.svg'} fill />
               )}
             </div>
-            <div>
-              <p className="text-lg">
-                {status
-                  ? `Sua conta foi confirmada com sucesso! Você será redirecionado para o login em ${timeout} segundos`
-                  : `Houve um erro ao confirmar a sua conta. Tente o cadastro novamente`}
-              </p>
+            <div className='"w-full  flex items-center justify-center'>
+              {status ? (
+                <p className=" text-lg font-semibold">
+                  Sua conta foi confirmada com sucesso! Você será redirecionado
+                  para o login em{' '}
+                  <span className="font-black text-primary-400">{timeout}</span>{' '}
+                  segundos
+                </p>
+              ) : (
+                <p className=" text-lg font-semibold w-[80%] text-center ">
+                  Houve um erro ao confirmar a sua conta. Tente o cadastro
+                  novamente{' '}
+                  <Link
+                    href={'/auth/register'}
+                    className="text-secundary-500 underline animate-pulse"
+                  >
+                    Clique Aqui
+                  </Link>
+                </p>
+              )}
             </div>
-          </>
+          </div>
         )}
       </main>
     </>
