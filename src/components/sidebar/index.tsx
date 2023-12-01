0fa2e60a04b7f5f83508d55e8d849a8516bd7fdb
Felipe Souza <j.felipe.dev.18v@gmail.com>
2023-12-01 16:43:19 -0300

    update

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -10,15 +10,17 @@ const Sidebar: React.FC = () => {
   return (
     <aside
       id="default-sidebar"
-      className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+      className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:${
+        status ? 'translate-x-full' : 'translate-x-0'
+      }  `}
       aria-label="Sidebar"
     >
-      <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+      <div className="h-full px-3 py-4 overflow-y-auto bg-secundary-400 ">
         <ul className="space-y-2 font-medium">
           <li>
             <Link
               href="/"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              className="flex items-center p-2 rounded-lg text-white hover:bg-gray-100  group"
             >
               <span className="ms-3">Dashboard</span>
             </Link>
